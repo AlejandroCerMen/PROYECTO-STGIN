7baffd64b483a35239ce990613667c674c91b463
Pablo Fernandez Albaladejo <pferalb@teleco.upv.es>
2026-01-24 22:55:23 +0100

    faltan pozo,carcel,posada,laberinto,inicio y fin

--- proyecto_oca.sql
+++ proyecto_oca.sql
@@ -19,59 +19,28 @@
 CREATE DATABASE IF NOT EXISTS `proyecto_oca` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `proyecto_oca`;
 
--- Volcando estructura para tabla proyecto_oca.detallespartida
-CREATE TABLE IF NOT EXISTS `detallespartida` (
-  `IdPartida` int(11) NOT NULL,
-  `IdJugador` int(11) NOT NULL,
-  `Casilla` int(11) DEFAULT 1,
-  `Orden` int(11) DEFAULT NULL,
-  `Bloqueo` int(11) DEFAULT 0,
-  `CasillaActual` int(11) DEFAULT 1,
-  `TurnosCastigos` int(11) DEFAULT 0,
-  PRIMARY KEY (`IdPartida`,`IdJugador`),
-  KEY `IdJugador` (`IdJugador`),
-  CONSTRAINT `detallespartida_ibfk_1` FOREIGN KEY (`IdPartida`) REFERENCES `partidas` (`IdPartida`) ON DELETE CASCADE,
-  CONSTRAINT `detallespartida_ibfk_2` FOREIGN KEY (`IdJugador`) REFERENCES `jugadores` (`IdJugador`) ON DELETE CASCADE
+-- Volcando estructura para tabla proyecto_oca.mensajes
+CREATE TABLE IF NOT EXISTS `mensajes` (
+  `IdMensaje` int(11) NOT NULL,
+  `TextoTemplate` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`IdMensaje`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla proyecto_oca.detallespartida: ~35 rows (aproximadamente)
-DELETE FROM `detallespartida`;
-INSERT INTO `detallespartida` (`IdPartida`, `IdJugador`, `Casilla`, `Orden`, `Bloqueo`, `CasillaActual`, `TurnosCastigos`) VALUES
-	(13, 1, 1, 1, 0, 24, 0),
-	(13, 2, 1, 2, 0, 22, 0),
-	(14, 1, 1, 2, 0, 10, 0),
-	(14, 3, 1, 1, 0, 7, 0),
-	(15, 1, 1, 1, 0, 10, 0),
-	(15, 2, 1, 4, 0, 15, 0),
-	(15, 4, 1, 2, 0, 26, 0),
-	(15, 5, 1, 3, 0, 22, 0),
-	(40, 5, 1, 2, 0, 1, 0),
-	(40, 9, 1, 1, 0, 1, 0),
-	(41, 5, 1, 1, 0, 13, 0),
-	(41, 9, 1, 2, 0, 11, 0),
-	(42, 9, 1, 1, 0, 1, 0),
-	(43, 5, 1, 1, 0, 1, 0),
-	(43, 9, 1, 2, 0, 1, 0),
-	(44, 5, 1, 1, 0, 1, 0),
-	(44, 9, 1, 2, 0, 1, 0),
-	(45, 5, 1, 2, 0, 1, 0),
-	(45, 9, 1, 1, 0, 9, 0),
-	(46, 5, 1, 1, 0, 63, 0),
-	(46, 9, 1, 2, 0, 56, 0),
-	(47, 5, 1, 1, 0, 4, 0),
-	(47, 9, 1, 2, 0, 2, 0),
-	(48, 5, 1, 1, 0, 2, 0),
-	(48, 9, 1, 2, 0, 1, 0),
-	(49, 5, 1, 1, 0, 63, 0),
-	(49, 9, 1, 2, 0, 51, 0),
-	(50, 5, 1, 1, 0, 26, 0),
-	(50, 9, 1, 2, 0, 85, 0),
-	(51, 5, 1, 1, 0, 57, 0),
-	(51, 9, 1, 2, 0, 57, 0),
-	(52, 5, 1, 1, 0, 1, 0),
-	(52, 9, 1, 2, 0, 1, 0),
-	(53, 5, 1, 1, 0, 58, 0),
-	(53, 9, 1, 2, 0, 58, 0);
+-- Volcando datos para la tabla proyecto_oca.mensajes: ~12 rows (aproximadamente)
+DELETE FROM `mensajes`;
+INSERT INTO `mensajes` (`IdMensaje`, `TextoTemplate`) VALUES
+	(0, '¡{1} EMPIEZA EL JUEGO! '),
+	(1, '{1} ha sacado un {0}'),
+	(2, '¡De Oca a Oca! {1} salta a la casilla {0}'),
+	(3, '¡De Puente a Puente! y {1} tira por la corriente'),
+	(4, '¡Posada! {1} se quedará a descansar hasta que pase alguien'),
+	(5, '¡POZO! {1} se quedará hasta que alguien le ayude'),
+	(6, '¡FIN DEL JUEGO! {1} ha llegado a la meta'),
+	(7, '¡{1} A LA CARCEL POR SINVERGÜENZA! hasta que lo rescaten'),
+	(8, '¡LA MUERTE! {1} vuelve a empezar'),
+	(9, '¡LABERINTO! {1} se ha perdido durante {4} turnos'),
+	(10, '{1} ha caido en los dados'),
+	(11, '¡{1} SE TORRÓ! retrocede a la casilla {0} ');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
